d74f525202f176f14c8e67c60e369a994357afa4
Nico Travassos <me@nicotravassos.co.za>
2023-04-24 20:19:34 +0200

    refactor: improve fetchData function and code organization

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let tabledata = [];
+let tableData = [];
 
 function fetchData() {
   const apiKey = document.getElementById("api-key").value;
@@ -16,42 +16,20 @@ function fetchData() {
   fetch(dataUrl)
     .then((response) => response.json())
     .then((data) => {
-      tabledata = data;
-      if (tabledata.length === 0) {
-        const noDataMessage = document.getElementById("no-data-message");
-        noDataMessage.classList.remove("hidden");
-        const dataTable = document.getElementById("data-table");
-        dataTable.classList.add("hidden");
+      tableData = data;
+      if (tableData.length === 0) {
+        displayNoDataMessage();
         return;
       }
 
-      tabledata.forEach((row) => {
+      tableData.forEach((row) => {
         const apiUrl = `https://api.torn.com/user/${row.id}?selections=basic&key=${apiKey}`;
         fetch(apiUrl)
           .then((response) => response.json())
           .then((userData) => {
-            let status = userData.status.state;
-            if (status === "Hospital") {
-              const remaining = userData.status.until - Date.now() / 1000;
-              const minutes = Math.floor(remaining / 60);
-              const seconds = Math.floor(remaining % 60);
-              status = `Hospitalized (${minutes}m ${seconds}s)`;
-            }
-            const attackLink = `<a href="https://www.torn.com/loader2.php?sid=getInAttack&user2ID=${row.id}">Attack Link</a>`;
-            const newRow = `
-              <tr>
-                <td class="pl-2"><a href="https://www.torn.com/profiles.php?XID=${row.id}" target="_blank">${row.name}</a></td>
-                <td>${row.id}</td>
-                <td>${row.lvl}</td>
-                <td>${row.total}</td>
-                <td>${row.str}</td>
-                <td>${row.def}</td>
-                <td>${row.spd}</td>
-                <td>${row.dex}</td>
-                <td>${status}</td>
-                <td class="text-blue-300">${attackLink}</td>
-              </tr>
-            `;
+            const status = formatStatus(userData.status);
+            const attackLink = createAttackLink(row.id);
+            const newRow = createTableRow(row, status, attackLink);
             tableBody.innerHTML += newRow;
           })
           .catch((error) => {
@@ -59,12 +37,59 @@ function fetchData() {
           });
       });
 
-      const noDataMessage = document.getElementById("no-data-message");
-      noDataMessage.classList.add("hidden");
-      const dataTable = document.getElementById("data-table");
-      dataTable.classList.remove("hidden");
+      hideNoDataMessage();
+      displayDataTable();
     })
     .catch((error) => {
       console.error("Error fetching data:", error);
     });
+}
+
+function displayNoDataMessage() {
+  const noDataMessage = document.getElementById("no-data-message");
+  noDataMessage.classList.remove("hidden");
+  const dataTable = document.getElementById("data-table");
+  dataTable.classList.add("hidden");
+}
+
+function hideNoDataMessage() {
+  const noDataMessage = document.getElementById("no-data-message");
+  noDataMessage.classList.add("hidden");
+}
+
+function displayDataTable() {
+  const dataTable = document.getElementById("data-table");
+  dataTable.classList.remove("hidden");
+}
+
+function formatStatus(status) {
+  let formattedStatus = status.state;
+  if (formattedStatus === "Hospital") {
+    const remaining = status.until - Date.now() / 1000;
+    const minutes = Math.floor(remaining / 60);
+    const seconds = Math.floor(remaining % 60);
+    formattedStatus = `Hospitalized (${minutes}m ${seconds}s)`;
+  }
+  return formattedStatus;
+}
+
+function createAttackLink(id) {
+  return `<a href="https://www.torn.com/loader2.php?sid=getInAttack&user2ID=${id}">Attack Link</a>`;
+}
+
+function createTableRow(row, status, attackLink) {
+  return `
+    <tr>
+      <td class="pl-2"><a href="https://www.torn.com/profiles.php?XID=${row.id}" target="_blank">${row.name}</a></td>
+      <td>${row.id}</td>
+      <td>${row.lvl}</td>
+      <td>${row.total}</td>
+      <td>${row.str}</td>
+      <td>${row.def}</td>
+      <td>${row.spd}</td>
+      <td>${row.dex}</td>
+      <td>${status}</td>
+      <td class="text-blue-300">${attackLink}</td>
+    </tr>
+  `;
 }
